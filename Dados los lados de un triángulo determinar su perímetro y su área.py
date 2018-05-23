b389b92ede6c9b4da3bae7fe44721346116ce4f4
PercyRodriguez <39410859+PercyRodriguez@users.noreply.github.com>
2018-05-23 10:38:54 -0400

    Add files via upload

---
+++ Dados los lados de un triángulo determinar su perímetro y su área.py
@@ -0,0 +1,25 @@
+#Dados los lados de un triángulo determinar su perímetro y su área
+a = int(input("ingrese primer lado"))
+b = int(input("ingrese segundo lado"))
+c = int(input("ingrese tercer lado"))
+
+if (a+b)>c and (a+c)>b and (b+c)>a:
+    p=a+b+c
+    s=b/2
+    area=(s(s*(s-a)*(s-b)*(s-c))**.5
+    if a != b and b != c and a != c :
+        print ("Triangulo escaleno de perimetro" ,p, "y area =",a)
+       #llamar graficador
+    else:
+       if(a == b) and (b == c)
+          print ("Triangulo equilatero de perimetro" ,p, "y area =",a)
+        else:
+          print ("Triangulo isoceles de perimetro" ,p, "y area =",a)
+else:
+          print("No es triangulo")
+
+def Triangulo (a,b,c):
+    program statement 1
+    program statement 2
+    program statement 3
+          
